e5c8b235c540cf888b7bbb41a2e19a764687fe23
Andrei <n3rd4i@gmail.com>
2019-06-22 16:23:28 +0200

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. "$toolsDir\commonEnv.ps1"
+
+$url = 'https://zdoom.org/files/gzdoom/bin/gzdoom-bin-4-1-3-x64.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$installLocation" # Install-ChocolateyZipPackage
+  url           = $url # download url, HTTPS preferred
+  softwareName  = 'gzdoom*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '139DC50192E1A451D7710703FC7E54B4' # %chocolateyinstall%\tools\checksum.exe
+}
+Install-ChocolateyZipPackage @packageArgs
+
+## StartMenu
+Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\$Name.lnk" `
+  -TargetPath "$installLocation\$gzdoomBin" `
+  -WorkingDirectory "$installLocation"
+
+## Desktop
+# Install-ChocolateyShortcut -ShortcutFilePath "$lnkDesktop" `
+#   -TargetPath "$installLocation\$gzdoomBin" `
+#   -WorkingDirectory "$installLocation"
---
+++ tools/commonEnv.ps1
@@ -0,0 +1,7 @@
+## Common between Install and Uninstall
+$Name = 'GZDoom'
+$installLocation = "$ENV:LocalAppData\Programs\gzdoom"
+$startMenuDir = "$ENV:AppData\Microsoft\Windows\Start Menu\Programs\$Name"
+$lnkDesktop = "$ENV:UserProfile\Desktop\$Name.lnk"
+
+$gzdoomBin = 'gzdoom.exe'
