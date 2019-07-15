a7c36ee6b61579c8d1cb4353585fc74374a0146c
Andrei <n3rd4i@gmail.com>
2019-07-15 18:13:23 +0200

    fixes after review comments

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://zdoom.org/files/gzdoom/bin/gzdoom-bin-4-1-3-x64.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = "$installLocation" # Install-ChocolateyZipPackage
-  url           = $url # download url, HTTPS preferred
-  softwareName  = 'gzdoom*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  unzipLocation = "$installLocation"
+  url           = $url
+  softwareName  = 'gzdoom*'
   checksum      = '139DC50192E1A451D7710703FC7E54B4' # %chocolateyinstall%\tools\checksum.exe
 }
 Install-ChocolateyZipPackage @packageArgs
