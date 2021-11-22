e9155e6473b1d454f4954a8eb78ac25aad14c72e
Andrei Hutanu <andrei.hutanu@evalan.com>
2021-11-22 22:27:51 +0100

    updated to 4.7.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$installLocation"
-  url           = 'https://github.com//coelckers/gzdoom/releases/download/g4.7.0/gzdoom-4-7-0-Windows-64bit.zip'
-  checksum      = '13471bc1f5bb2dfa855056731bd0bc5b7ebc1e11ea9a00645df4d9b942d89b83'
+  url           = 'https://github.com//coelckers/gzdoom/releases/download/g4.7.1/gzdoom-4-7-1-Windows-64bit.zip'
+  checksum      = 'ca63e508d0444068dbfc1317fa02c565c15e72649e78849b0fea0772fc66dc12'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
